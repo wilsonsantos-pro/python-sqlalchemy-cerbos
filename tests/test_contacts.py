36ff9f81a210597aa887151b7f0157af58609916
Wilson Santos <wilson.santos.dev87@gmail.com>
2023-01-23 20:47:59 +0100

    Added test for create new contact

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1,13 +1,24 @@
 # pylint: disable=redefined-outer-name,unused-argument
-from typing import TYPE_CHECKING, Any, Dict, List
+from typing import TYPE_CHECKING, Any, Dict, Generator, List
 
 import pytest
+from sqlalchemy import delete
+
+from app.models import Contact, Session, User
+from app.schemas import ContactSchema
 
 if TYPE_CHECKING:
     from fastapi.testclient import TestClient
 
 Username = str
 ContactDict = Dict[str, Any]
+
+
+@pytest.fixture
+def headers() -> Dict:
+    return {
+        "accept": "application/json",
+    }
 
 
 @pytest.fixture
@@ -65,13 +76,47 @@ def expected_contacts_for() -> Dict[Username, List[ContactDict]]:
 @pytest.mark.parametrize("username", ["john", "geri"])
 def test_get_contacts(
     client: "TestClient",
+    headers: Dict,
     username: str,
     expected_contacts_for: Dict[Username, List[ContactDict]],
 ):
-    headers = {
-        "accept": "application/json",
-    }
     auth = (username, "")
     response = client.get("/contacts", headers=headers, auth=auth)
     assert response.status_code == 200, response.json()
     assert response.json() == expected_contacts_for[username]
+
+
+@pytest.fixture
+def new_contact() -> Generator:
+    new_contact = ContactSchema(
+        first_name="Charlie",
+        last_name="Harper",
+        owner_id="1",
+        company_id="2",
+        is_active=True,
+        marketing_opt_in=False,
+    )
+    yield new_contact
+    with Session() as dbsession:
+        dbsession.execute(
+            delete(Contact).where(
+                Contact.first_name == new_contact.first_name,
+                Contact.last_name == new_contact.last_name,
+            )
+        )
+        dbsession.commit()
+
+
+@pytest.mark.parametrize("username,expected", [("john", 200), ("geri", 403)])
+def test_create_new_contact(
+    client: "TestClient",
+    headers: Dict,
+    username: str,
+    expected: int,
+    new_contact: ContactSchema,
+):
+    auth = (username, "")
+    response = client.post(
+        "/contacts/new", headers=headers, auth=auth, json=new_contact.dict()
+    )
+    assert response.status_code == expected, response.json()
